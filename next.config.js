85f65ca57814907f37a8bcc0aa0f392ba5d5b587
BLACHIA <blacknigaless@gmail.com>
2026-06-29 23:05:45 +0200

    Use vercel.json for CSP headers, remove from next.config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; style-src 'unsafe-inline' 'self'; script-src 'unsafe-inline' 'self' 'unsafe-eval'; img-src 'self' data:; font-src 'self' data:;",
-          },
-        ],
-      },
-    ];
-  },
-  // Autoriser les images et polices à partir de sources externes si besoin
+  // Autoriser es images et polices à partir de sources externes si besoin
 };
 
 module.exports = nextConfig;
